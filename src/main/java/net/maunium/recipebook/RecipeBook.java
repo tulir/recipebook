9eca4903eb99992e7ecd13cf2b2248b5349a41b1
Tulir Asokan <tulir@maunium.net>
2017-10-13 16:30:44 +0300

    Update things

--- src/main/java/net/maunium/recipebook/RecipeBook.java
+++ src/main/java/net/maunium/recipebook/RecipeBook.java
@@ -1,7 +1,9 @@
 package net.maunium.recipebook;
 
+import spark.Spark;
+
 public class RecipeBook {
     public static void main(String[] args) {
-
+        Spark.get("/hello", (req, res) -> "Hello World");
     }
 }
