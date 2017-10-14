6dc429aa00d91801acf8b768ec750e615fb4a1a9
Tulir Asokan <tulir@maunium.net>
2017-10-14 13:48:55 +0300

    Add javadocs for all utility classes

--- src/main/java/net/maunium/recipebook/util/ErrorMessage.java
+++ src/main/java/net/maunium/recipebook/util/ErrorMessage.java
@@ -3,11 +3,20 @@ package net.maunium.recipebook.util;
 /**
  * ErrorMessage is a simple error message intended for JSON serialization.
  *
+ * This is intended to be returned from API endpoint handlers that use the
+ * {@link JSON#transformer()} response transformer.
+ *
  * @author Tulir Asokan
  * @project RecipeBook
  */
 public class ErrorMessage {
+	/**
+	 * The state of the app when this message is returned.
+	 */
 	public final String state = "error";
+	/**
+	 * A description of the error that occurred.
+	 */
 	public String message;
 
 	public ErrorMessage(String message) {
--- src/main/java/net/maunium/recipebook/util/JSON.java
+++ src/main/java/net/maunium/recipebook/util/JSON.java
@@ -10,16 +10,34 @@ import spark.ResponseTransformer;
  * @project RecipeBook
  */
 public class JSON {
-	private static final Gson gson = new Gson();
+	/**
+	 * The GSON instance that is used by the static methods in this class.
+	 */
+	public static final Gson gson = new Gson();
 
+	/**
+	 * Convert an object into a JSON string.
+	 *
+	 * @param object The object to convert.
+	 * @return A JSON string.
+	 */
 	public static String toJSON(Object object) {
 		return gson.toJson(object);
 	}
 
+	/**
+	 * Parse a JSON string into an object of the given type.
+	 * @param json The JSON string.
+	 * @param type The class which to parse the JSON into.
+	 * @return The parsed object.
+	 */
 	public static <T> T parseJSON(String json, Class<T> type) {
 		return gson.fromJson(json, type);
 	}
 
+	/**
+	 * @return A Spark {@link ResponseTransformer} that converts responses to JSON.
+	 */
 	public static ResponseTransformer transformer() {
 		return JSON::toJSON;
 	}
