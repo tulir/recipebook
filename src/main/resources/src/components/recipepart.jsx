dfcf3d91168296a75e1f4f628769d98a63161d2c
Tulir Asokan <tulir@maunium.net>
2017-10-16 17:04:34 +0300

    Configure npm run start to proxy API requests to the API

---
+++ src/main/resources/src/components/recipepart.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Ingredient extends Component {
+	constructor(id, name) {
+		super()
+		this.id = id
+		this.name = name
+	}
+
+	render() {
+		return (
+			<div className="ingredient">
+
+			</div>
+		)
+	}
+}
+
+export default Ingredient
